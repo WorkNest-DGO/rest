8d8c4cfb3c835fb50e7422e816924900f256df73
FuedUni <92557905+FuedUni@users.noreply.github.com>
2025-07-02 11:48:17 -0600

    mas cambios de bd

--- utils/bd.sql
+++ utils/bd.sql
@@ -424,3 +424,12 @@ END;
 //
 
 DELIMITER ;
+
+
+-- mas cambios de bd
+ALTER TABLE ventas ADD COLUMN estado_entrega ENUM('pendiente', 'en_camino', 'entregado') DEFAULT 'pendiente',
+ADD COLUMN fecha_asignacion DATETIME DEFAULT NULL,
+ADD COLUMN fecha_inicio DATETIME DEFAULT NULL,
+ADD COLUMN fecha_entrega DATETIME DEFAULT NULL,
+ADD COLUMN seudonimo_entrega VARCHAR(100) DEFAULT NULL,
+ADD COLUMN foto_entrega VARCHAR(255) DEFAULT NULL;
