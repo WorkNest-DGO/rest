cfdd791698a8e4c3974daeeff9c25c66f177d1ea
FuedUni <92557905+FuedUni@users.noreply.github.com>
2025-07-17 18:07:26 -0600

    Update bd.sql

--- utils/bd.sql
+++ utils/bd.sql
@@ -1,23 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 17-07-2025 a las 15:26:45
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
+-- activar extension=gd en php.ini 
 -- Base de datos: `restaurante`
 --
 
