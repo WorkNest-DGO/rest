8b2eef4ef485bc5772cf2940dead7cce35936751
FuedUni <92557905+FuedUni@users.noreply.github.com>
2025-07-22 13:55:40 -0600

    front

--- vistas/mesas/mesas2.php
+++ vistas/mesas/mesas2.php
@@ -9,6 +9,12 @@ if (!in_array($path_actual, $_SESSION['rutas_permitidas'])) {
 $title = 'Mesas';
 ob_start();
 ?>
+<!-- Dragula CSS -->
+<link rel="stylesheet" href="https://unpkg.com/dragula@3.7.2/dist/dragula.min.css">
+
+<!-- Dragula JS -->
+<script src="https://unpkg.com/dragula@3.7.2/dist/dragula.min.js"></script>
+
 <link href="../../utils/css/style2.css" rel="stylesheet">
 <div class="page-header mb-0">
     <div class="container">
@@ -31,6 +37,36 @@ ob_start();
 </div>
 <div id="tablero"></div>
 <div id="modal-detalle" style="display:none;"></div>
+
+<div>
+<section class="section">
+	<h1>Kanban Drag and Drop Interface Layout</h1>
+	<h4>Inspired by <a href="https://trello.com/
+    ">Trello</a>, and <a href="https://www.google.com/keep/">Google Keep</a>, <a href="http://blog.invisionapp.com/design-project-management-tool/">Invision</a> and <a href="https://twitter.com/aaronstump">@aaronstump</a></h4>
+</section>
+
+<div class="drag-container">
+	<ul class="drag-list">
+		<li class="drag-column drag-column-on-hold">
+			<span class="drag-column-header">
+				<h2>On Hold</h2>
+				<svg class="drag-header-more" data-target="options1" fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/</svg>
+			</span>
+				
+			<div class="drag-options" id="options1"></div>
+			
+			<ul class="drag-inner-list" id="1">
+				<li class="drag-item"></li>
+				<li class="drag-item"></li>
+			</ul>
+		</li>
+
+	</ul>
+</div>
+
+</div>
+
+
 
 
 <?php require_once __DIR__ . '/../footer.php'; ?>
